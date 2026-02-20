a79762eec2a3bb20d488af5ab0424dfddfeb60d7
Bhavik Jain <bhavikjain9290@gmail.com>
2026-02-20 09:16:22 +0530

    UC4: Character array based palindrome check

---
+++ src/U4.java
@@ -0,0 +1,29 @@
+public class U4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("UC4 - Character Array Based Palindrome Check");
+
+        String input = "radar";
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Result: Palindrome");
+        } else {
+            System.out.println("Result: Not Palindrome");
+        }
+    }
+}
